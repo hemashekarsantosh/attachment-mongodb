8ded3147d0eb5a622b0de7307bcb60d99d0dc15a
Santosh Hemashekar <hemashekarsantosh@gmail.com>
2022-02-04 09:18:29 +0000

    fix

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
 const server = http.createServer(app);
 
 server.listen(port,() => console.log('Server started on port ' + port) );
-console.log('Open Shift Node JS PORT '+OPENSHIFT_NODEJS_PORT);
+console.log('Open Shift Node JS PORT '+process.env.OPENSHIFT_NODEJS_PORT);
 console.log('NODE PORT'+process.env.NODE_PORT);
